2269fa20d2c1bed7475c403ee9e34a31e8e66f76
Steele Shreve <steele@steelesh.dev>
2025-04-21 20:57:47 -0500

    feat: scaffold frontend (sveltekit)

---
+++ frontend/src/routes/page.svelte.test.ts
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/svelte";
+import "@testing-library/jest-dom/vitest";
+import { describe, expect } from "vitest";
+
+import Page from "./+page.svelte";
+
+describe("/+page.svelte", () => {
+  it("should render h1", () => {
+    render(Page);
+    expect(screen.getByRole("heading", { level: 1 })).toBeInTheDocument();
+  });
+});
